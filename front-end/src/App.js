7debf65550777d9451263c0759a9849f88c29c2a
HericMendez <heric.mendez00@gmail.com>
2022-02-21 20:39:26 -0300

    front-end recebendo dados pela rota get

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,35 +1,64 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddClienteForm from "./components/forms/AddClienteForm";
 import EditaClienteForm from "./components/forms/EditaClienteForm";
 import TabelaClientes from "./components/tables/TabelaClientes";
-
+import Api from "./api";
 
 const App = () => {
-  const DadosClientes = [
-    { id: 1, nome: "Tania", cpf: "12345678900" },
-    { id: 2, nome: "Craig", cpf: "32165498710" },
-    { id: 3, nome: "Ben", cpf: "11122233344" },
-  ];
+
+  useEffect(() => {
+    getClientApi();
+    
+    console.log("renderizou");
+  }, []);
   const initialFormState = { id: null, nome: "", cpf: "" };
-  const [clientes, setClientes] = useState(DadosClientes);
+  
+
+
+  let clientsData = [];
+  const [clientes, setClientes] = useState(clientsData);
+
   const [modoEdicao, setModoEdicao] = useState(false);
-
-
   const [clienteAtual, setClienteAtual] = useState(initialFormState);
 
+  async function getClientApi(){
+   await Api.get(`client/`)
+      .then((res) => {
+        console.log("data from API:", res.data);
+        if(res.data == null)
+				clientsData = [];
+			else
+				clientsData = res.data;
+
+			setClientes(clientsData);
+
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  console.log(clientes)
   const addCliente = (cliente) => {
     cliente.id = clientes.length + 1;
     setClientes([...clientes, cliente]);
   };
 
   const editaCliente = (cliente) => {
+
+
+    
     setModoEdicao(true);
     setClienteAtual({ id: cliente.id, nome: cliente.nome, cpf: cliente.cpf });
+
   };
 
-  const atualizaCliente =(id, clienteAtualizado)=>{
-    setModoEdicao(false)
-    setClientes(clientes.map((cliente)=>(cliente.id===id ? clienteAtualizado : cliente)));
+  const atualizaCliente = (id, clienteAtualizado) => {
+    setModoEdicao(false);
+    setClientes(
+      clientes.map((cliente) =>
+        cliente.id === id ? clienteAtualizado : cliente
+      )
+    );
 
     /*
     which means: 
@@ -37,7 +66,7 @@ const App = () => {
     se for igual à do estado atual, altere (cliente atualizado); 
     se não for, mantenha o valor atual (cliente). 
     */
-  }
+  };
 
   const deletaCliente = (id) => {
     setClientes(clientes.filter((cliente) => cliente.id !== id));
@@ -47,22 +76,22 @@ const App = () => {
     <div className="container">
       <h1>React Frontend</h1>
       <div className="flex-row">
-      <div className="flex-large">
-      {modoEdicao ? (
-        <div>
-          <h2>Atualizar Cadastro</h2>
-          <EditaClienteForm 
-          modoEdicao={modoEdicao}
-          clienteAtual={clienteAtual}
-          atualizaCliente={atualizaCliente}
-          />
-        </div>
-        ):(
-          <div>
-            <h2>Novo Cliente</h2>
-          <AddClienteForm addCliente={addCliente} />
-          </div>
-      )}
+        <div className="flex-large">
+          {modoEdicao ? (
+            <div>
+              <h2>Atualizar Cadastro</h2>
+              <EditaClienteForm
+                modoEdicao={modoEdicao}
+                clienteAtual={clienteAtual}
+                atualizaCliente={atualizaCliente}
+              />
+            </div>
+          ) : (
+            <div>
+              <h2>Novo Cliente</h2>
+              <AddClienteForm addCliente={addCliente} />
+            </div>
+          )}
         </div>
         <div className="flex-large">
           <h2>Lista de Clientes</h2>
@@ -78,7 +107,6 @@ const App = () => {
 };
 
 export default App;
-
 
 /*
 
---
+++ front-end/src/api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+const baseUrl = 'http://localhost:8080/';
+
+axios.defaults.baseURL=baseUrl;
+
+
+export default axios;
+
